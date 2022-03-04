7a1f3ad80db022a4d1ce40b1225e8cd143a51428
Shruti Math <shruti.math@quantiphi.com>
2022-03-04 17:27:52 +0530

    classification skeleton added

--- microservices/classification_service/src/routes/classification_test.py
+++ microservices/classification_service/src/routes/classification_test.py
@@ -6,7 +6,6 @@ import os
 # pylint: disable=unused-argument,redefined-outer-name,unused-import
 from testing.fastapi_fixtures import client_with_emulator
 from common.testing.firestore_emulator import firestore_emulator, clean_firestore
-# from .config_test import  TESTDATA_FILENAME1 ,TESTDATA_FILENAME2
 
 # assigning url
 api_url = "http://localhost:8080/classification_service/v1/classification/"
@@ -15,8 +14,8 @@ os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8080"
 os.environ["GOOGLE_CLOUD_PROJECT"] = "fake-project"
 SUCCESS_RESPONSE = {"status": "Success"}
 
-
 def test_classification_api(client_with_emulator):
+  """Test case to check the classification endpoint"""
 
   response = client_with_emulator.post(
       f"{api_url}classification_api?case_id=123A&uid=232a"
