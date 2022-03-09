5f58c709acc1488545e23fe30386e3dce28edbc2
Shruti Math <shruti.math@quantiphi.com>
2022-03-09 07:42:59 +0530

    document collection structure updated

--- common/src/common/models/document_status.py
+++ common/src/common/models/document_status.py
@@ -3,29 +3,31 @@ Document Status object in the ORM
 """
 import os
 from common.models import BaseModel
-from fireo.fields import TextField, DateTime ,ListField
+from fireo.fields import TextField, DateTime ,ListField ,NumberField
 
 DATABASE_PREFIX = os.getenv("DATABASE_PREFIX", "")
 PROJECT_ID = os.environ.get("PROJECT_ID", "")
 
-
-# sample class
-class Documentstatus(BaseModel):
+class Document(BaseModel):
   """Documentstatus ORM class
   """
   case_id = TextField()
   uid = TextField()
   url = TextField()
-  state = TextField()
-  type_of_doc = TextField()
-  class_of_doc = TextField()
+  document_type  = TextField()
+  document_class = TextField()
+  context = TextField()
   system_status = ListField()
-  status_timestamp = ListField()
+  hitl_status = ListField()
   upload_time = DateTime()
   active = TextField()
-  hitl_status = TextField()
-  hitl_update_time =  ListField()
+  upload_timestamp = DateTime()
+  entities = ListField()
+  extraction_score = NumberField()
+  validation_score = NumberField()
+  matching_score = NumberField()
+  auto_approval = NumberField()
 
   class Meta:
     ignore_none_field = False
-    collection_name = DATABASE_PREFIX+ "document_status"
+    collection_name = DATABASE_PREFIX+ "document"
