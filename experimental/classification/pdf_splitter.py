4ca40fc27a79c3ddc84d5baf457882c493ea34b9
SumitQ <97742524+SumitQ@users.noreply.github.com>
2022-03-15 14:23:39 -0400

    linting corrected

--- experimental/classification/pdf_splitter.py
+++ experimental/classification/pdf_splitter.py
@@ -1,103 +1,88 @@
+"""
+Module to perform operations on pdf files
+"""
 from datetime import datetime
 import os
-import argparse
-from PyPDF2 import PdfFileReader, PdfFileWriter, pdf
-try:
-    import fitz
-except:
-    print(__file__, "needs PyMuPDF (fitz).")
-    raise SystemExit
+import sys
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import fitz
 
-if not tuple(map(int, fitz.version[0].split("."))) >= (1, 18, 18):
-    raise SystemExit("require PyMuPDF v1.18.18+")
-
+if tuple(map(int, fitz.version[0].split('.'))) < (1, 18, 18):
+	raise SystemExit('require PyMuPDF v1.18.18+')
 
 class PDFManager:
-
-    def __init__(self, pdf_file, out_path):
-        self.inp_file_pdf = pdf_file
-        self.out_path = out_path        
-        self.timestamp = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-        self.inp_doc = fitz.open(self.inp_file_pdf)
-
-    def split_and_save2pdf(self) -> list :
-        """ 
-        Splits all the pages of pdf file and save them to pdf
-        Returns:
-        """
-        with open(self.inp_file_pdf, 'rb') as fd:
-            reader_obj = PdfFileReader(fd)
-            for i in range(reader_obj.numPages):
-                self.save_to_pdf(i, reader_obj.getPage(i))
-    
-    def split_save2img(self, page_num=0, save=True):
-        """ 
-        Generates an image for the first page of the PDF doc
-        Returns:
-        """
-        if page_num < self.inp_doc.page_count:
-            return self.save2image(page_num, self.inp_doc.load_page(page_num), save)
-        else:
-            print("Page number doesnot exist")
-            exit(-1)
-
-    def split_all_save2img(self, save=True):
-        """ 
-        Splits all the pages into images.
-        Returns:
-        """
-
-        for pg_num in range(self.inp_doc.page_count):
-            self.split_save2img(self, pg_num, save=True)
+	"""A class to perform operations on a pdf file.
+	"""
+	def __init__(self, pdf_file, out_path):
+		self.inp_file_pdf = pdf_file
+		self.out_path = out_path
+		self.timestamp = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+		self.inp_doc = fitz.open(self.inp_file_pdf)
 
 
-    def save2image(self,pg_num, page, save):
-        file_name = self.timestamp + '_' + os.path.basename(self.inp_file_pdf)
-        pix = page.get_pixmap()
-        output = os.path.join(
-            self.out_path, file_name[:-4] + "_%s.png"% str(pg_num))    
-        if save: 
-            pix.save(output)
-            return output
-        else:
-            return page
-        
-    
-    def save_to_pdf(self, page_num, page):
-
-        writer = PdfFileWriter()
-        writer.addPage(page)
-
-        file_name = os.path.basename(self.inp_file_pdf)[:-4] + '_' + self.timestamp + '.pdf'
-        file_name = os.path.join(self.out_path, file_name[:-4] + "%s.pdf"% str(page_num))
-
-        with open(file_name, "wb") as out_stream:
-            writer.write(out_stream)
+	def split_and_save2pdf(self) -> list:
+		"""
+		Splits all the pages of pdf file and save them to pdf
+		Returns:
+		"""
+		with open(self.inp_file_pdf, 'rb') as fd:
+			reader_obj = PdfFileReader(fd)
+			for i in range(reader_obj.numPages):
+				self.save_to_pdf(i, reader_obj.getPage(i))
 
 
-def get_parser():
-  # Read command line arguments
-    parser = argparse.ArgumentParser(
-      formatter_class=argparse.RawTextHelpFormatter,
-      description="""
-      Script used to generate pdf/images for each pages in a pdf.
-      """,
-      epilog="""
-      Examples:
-      python pdf_splitter.py --inp_pdf_path=path-to-file.pdf --output=path-to-output-folder
-      """)
-    parser.add_argument(
-        "--inp_pdf_path",
-        default="data/Illinois_filled.pdf",
-        help="Path to input pdf file")
-    parser.add_argument(
-        "--output",
-        default="data/images",
-        help="Path to the destination output pdf/image file")
-    
-    parser.add_argument(
-        "--split_into",
-        default="images",
-        help="Type of conversion need on pages of a pdf file")
+	def split_save2img(self, page_num=0):
+		"""
+		Generates an image for the first page of the PDF doc
+		Returns:
+		"""
+		if page_num < self.inp_doc.page_count:
+			return self.save2image(page_num, self.inp_doc.load_page(page_num), True)
+		else:
+			print('Page number doesnot exist')
+			sys.exit()
 
-    return parser
+	def split_all_save2img(self):
+		"""
+		Splits all the pages into images.
+		Returns:
+		"""
+		for pg_num in range(self.inp_doc.page_count):
+			self.split_save2img(pg_num)
+
+	def save2image(self, pg_num, page, save):
+		"""
+		save a page in image format
+
+		Args:
+		pg_num (int): page number
+		page (pdf): page in pdf object type to be saved
+		save (bool): whether to save or not.
+
+		Returns:
+		str: page/image
+		"""
+		file_name = f'{self.timestamp}_{os.path.basename(self.inp_file_pdf)}'
+		pix = page.get_pixmap()
+		output = os.path.join(self.out_path, f'{file_name[:-4]}_{str(pg_num)}.png')
+		if save:
+			pix.save(output)
+			return output
+		else:
+			return page
+
+	def save_to_pdf(self, page_num, page):
+		"""
+		Save a page in pdf format
+
+		Args:
+		page_num (int): page number to be extracted and saved
+		page (pdf): page object to be saved
+		"""
+		writer = PdfFileWriter()
+		writer.addPage(page)
+		file_name = os.path.basename(self.inp_file_pdf)[:-4]
+		file_name = file_name + '_' + self.timestamp + '.pdf'
+		file_name = os.path.join(self.out_path, f'{file_name[:-4]}_{str(page_num)}.pdf')
+		with open(file_name, 'wb') as out_stream:
+			writer.write(out_stream)
--- microservices/classification_service/src/utils/classification/download_pdf_gcs.py
+++ microservices/classification_service/src/utils/classification/download_pdf_gcs.py
@@ -1,31 +1,37 @@
+"""Module to download files from GCS.
+"""
 from google.cloud import storage
-import os
 
 
-def download_pdf_gcs(bucket_name=None, gcs_uri=None, file_to_download=None, output_filename='gcs.pdf') -> str:
-    """Function takes a path of an object/file stored in GCS bucket and downloads
-    the file in the current working directory
+def download_pdf_gcs(
+  bucket_name=None, gcs_uri=None,
+  file_to_download=None, output_filename='gcs.pdf') -> str:
+  """
+  Function takes a path of an object/file stored in GCS bucket
+  and downloads
+  the file in the current working directory
+  Args:
+      bucket_name (str): bucket name from where file to be
+      downloaded Defaults to None.
+      gcs_uri (str): GCS object/file path. Defaults to None.
+      file_to_download (str): desired filename. Defaults to None.
+      output_filename (str): file_to_download = Y/ABC.txt.
+      Defaults to 'gcs.pdf'.
 
-    Args:
-        bucket_name (str): bucket name from where file to be downloaded
-        gcs_uri (str): GCS object/file path
-        output_filename (str): desired filename
-        file_to_download (str): gcs file path excluding bucket name.
-            Ex: if file is stored in X bucket under the folder Y with filename ABC.txt
-            then file_to_download = Y/ABC.txt
-    Return:
-        pdf_path (str): pdf file path that is downloaded from the bucket and stored in local
-    """
-    if bucket_name is None:
-        bucket_name = gcs_uri.split('/')[2]
-    
-    # if file to download is not provided it can be extracted from the GCS URI
-    if file_to_download is None and gcs_uri is not None:
-        file_to_download = '/'.join(gcs_uri.split('/')[3:])
+  Returns:
+      str: pdf file path that is downloaded from the bucket
+      and stored in local
+  """
+  if bucket_name is None:
+    bucket_name = gcs_uri.split('/')[2]
 
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(file_to_download)
+  # if file to download is not provided it can be extracted from the GCS URI
+  if file_to_download is None and gcs_uri is not None:
+    file_to_download = '/'.join(gcs_uri.split('/')[3:])
 
-    with open(output_filename, "wb") as file_obj:
-        blob.download_to_file(file_obj)
+  storage_client = storage.Client()
+  bucket = storage_client.get_bucket(bucket_name)
+  blob = bucket.blob(file_to_download)
+
+  with open(output_filename, 'wb') as file_obj:
+    blob.download_to_file(file_obj)
