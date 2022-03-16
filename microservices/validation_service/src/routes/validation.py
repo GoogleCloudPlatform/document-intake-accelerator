84ec16f54d15ed079c5786f7c7c1c234341fd83e
shamli.ingole <shamli.ingole@quantiphi.com>
2022-03-16 19:25:00 +0530

    Updated validation code

--- microservices/validation_service/src/routes/validation.py
+++ microservices/validation_service/src/routes/validation.py
@@ -14,41 +14,41 @@ FAILED_RESPONSE = {"status": "Failed"}
 
 @router.post("/validation_api")
 async def validation(case_id: str, uid: str, doc_class: str):
-    """ validates the document with case id , uid ,
-          Args:
-              case_id (str): Case id of the file ,
-               uid (str): unique id for  each document
-               doc_class (str): class of document
-          Returns:
-              200 : validation score successfully  updated
-              500  : HTTPException: 500 Internal Server Error if something fails
-        """
-    print(doc_class)
-    status = "fail"
-    doc = Document.find_by_uid(uid)
-    if not doc:
-        raise HTTPException(status_code=404, detail="uid not found")
-    try:
-        validation_score = get_values(doc_class, case_id, uid)
-        if validation_score:
-            status = "success"
-        # validation_status_update_url = f"http://document-status-service/document_status_service/v1/update_validation_status?case_id={case_id}&uid={uid}&validation_score={validation_score}&status={status}"
-        # response = requests.post(validation_status_update_url)
-        update_validation_status(case_id,uid,validation_score,status)
-        Logger.info(
-            f"Validation Score for cid:{case_id}, uid: {uid} is {validation_score}")
-        return {
-            "status": "success",
-            "score": f"{validation_score}"
-        }
-    except Exception as error:
-        print(error)
-        Logger.error(error)
-        raise HTTPException(
-            status_code=500, detail="Failed to update validation score") from error
+  """ validates the document with case id , uid , doc_class
+      Args:
+        case_id (str): Case id of the file ,
+         uid (str): unique id for  each document
+         doc_class (str): class of document
+      Returns:
+        200 : validation score successfully  updated
+        500  : HTTPException: 500 Internal Server Error if something fails
+    """
+  status = "fail"
+  doc = Document.collection.filter("uid", "==", uid).filter(
+    "case_id", "==", case_id).get()
+  if not doc:
+    Logger.error("uid or cid not found")
+    raise HTTPException(status_code=404, detail="uid or cid not found")
+  try:
+    validation_score = get_values(doc_class, case_id, uid)
+    if validation_score:
+      status = "success"
+    update_validation_status(case_id, uid, validation_score, status)
+    Logger.info(
+      f"Validation Score for cid:{case_id}, uid: {uid}, doc_class:{doc_class} is {validation_score}")
+    return {
+      "status": "success",
+      "score": f"{validation_score}"
+    }
+  except Exception as error:
+    print(error)
+    Logger.error(error)
+    raise HTTPException(
+      status_code=500, detail="Failed to update validation score") from error
 
 
 def update_validation_status(case_id, uid, validation_score, status):
-    base_url = "http://document-status-service/document_status_service/v1/update_validation_status"
-    req_url = f"{base_url}?case_id={case_id}&uid={uid}&validation_score={validation_score}&status={status}"
-    requests.post(req_url)
+  """Call status update api to update the validation score"""
+  base_url = "http://document-status-service/document_status_service/v1/update_validation_status"
+  req_url = f"{base_url}?case_id={case_id}&uid={uid}&validation_score={validation_score}&status={status}"
+  requests.post(req_url)
--- microservices/validation_service/src/routes/validation_test.py
+++ microservices/validation_service/src/routes/validation_test.py
@@ -18,30 +18,30 @@ SUCCESS_RESPONSE = {"status": "Success"}
 
 
 def test_validation_api(client_with_emulator):
-    """Test case to check the validation endpoint"""
-    doc = Document()
-    doc.case_id = "5-ui"
-    doc.uid = "aSCh3o6BxjPEqjMAQhtC"
-    doc.document_class = "driving_license"
-    doc.save()
-    url = f"{API_URL}validation_api?case_id=5-ui&uid=aSCh3o6BxjPEqjMAQhtC&doc_class=driving_license"
-    with mock.patch("routes.validation.update_validation_status"):
-      response = client_with_emulator.post(url)
-    assert response.status_code == 200, "Status 200"
-
-
-def test_validation_api_invalid_uid(client_with_emulator):
-    """Test case to check the validation endpoint when invalid uid provided"""
-    url = f"{API_URL}validation_api?case_id=5-ui&uid=1&doc_class=driving_license"
+  """Test case to check the validation endpoint"""
+  doc = Document()
+  doc.case_id = "5-ui"
+  doc.uid = "aSCh3o6BxjPEqjMAQhtC"
+  doc.document_class = "driving_license"
+  doc.save()
+  url = f"{API_URL}validation_api?case_id=5-ui&uid=aSCh3o6BxjPEqjMAQhtC&doc_class=driving_license"
+  with mock.patch("routes.validation.update_validation_status"):
     response = client_with_emulator.post(url)
-    assert response.status_code == 404, "Status 404"
+  assert response.status_code == 200, "Status 200"
+
+
+def test_validation_api_invalid_uid_cid(client_with_emulator):
+  """Test case to check the validation endpoint when invalid uid provided"""
+  url = f"{API_URL}validation_api?case_id=5-ui&uid=1&doc_class=driving_license"
+  response = client_with_emulator.post(url)
+  assert response.status_code == 404, "Status 404"
 
 def test_validation_api_invalid_doc_class(client_with_emulator):
-    """Test case to check the validation endpoint when invalid document class provided"""
-    doc = Document()
-    doc.case_id = "5-ui"
-    doc.uid = "aSCh3o6BxjPEqjMAQhtC"
-    doc.save()
-    url = f"{API_URL}validation_api?case_id=5-ui&uid=aSCh3o6BxjPEqjMAQhtC&doc_class=invalid_class"
-    response = client_with_emulator.post(url)
-    assert response.status_code == 500, "Status 500"
+  """Test case to check the validation endpoint when invalid document class provided"""
+  doc = Document()
+  doc.case_id = "5-ui"
+  doc.uid = "aSCh3o6BxjPEqjMAQhtC"
+  doc.save()
+  url = f"{API_URL}validation_api?case_id=5-ui&uid=aSCh3o6BxjPEqjMAQhtC&doc_class=invalid_class"
+  response = client_with_emulator.post(url)
+  assert response.status_code == 500, "Status 500"
