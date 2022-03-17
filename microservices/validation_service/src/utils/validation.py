318d856c44fb8a4d5e56349f1cb2169a04c1011a
Phulare <kshitij.phulare@quantiphi.com>
2022-03-17 11:35:29 +0530

    Resolved Linting Issue

--- microservices/validation_service/src/utils/validation.py
+++ microservices/validation_service/src/utils/validation.py
@@ -1,3 +1,7 @@
+'''
+This Script is Used to Calculate the Validation Score
+'''
+
 import json
 from google.cloud import bigquery
 from google.cloud import storage
@@ -36,12 +40,9 @@ def get_values(documentlabel,cid,uid):
     Validation Score
 
   '''
-  #Sample Values
-  # path =" https://storage.cloud.google.com/async_form_parser/Jsons/trial.json"
   path=PATH
   data=read_json(path)
-  # merge_query,documentlabel = get_values()
-  merge_query= " and cid='{}' and uid='{}' ".format(cid,uid)  
+  merge_query= f"and cid ='{cid}' and uid='{uid}' "
   validation_score = get_scoring(data,merge_query,documentlabel)
   # stats(data,validation_score,documentlabel)
   
@@ -64,9 +65,10 @@ def get_scoring(data,merge_query,documentlabel):
     query = data[documentlabel][i] + merge_query
     # query=i+d
     print(query)
-    Query_Results = bigquery_client.query((query))
-    df = Query_Results.to_dataframe()
+    query_results = bigquery_client.query((query))
+    df = query_results.to_dataframe()
     print(df)
     validation_score = validation_score + len(df)
   validation_score = validation_score/len(data[documentlabel])
   return validation_score/len(data[documentlabel])
+  
