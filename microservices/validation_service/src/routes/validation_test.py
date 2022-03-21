c1361c82781c796ac9f06c575227e5d84da92624
shamli.ingole <shamli.ingole@quantiphi.com>
2022-03-21 14:15:25 +0530

    Updated unit test case for validation

--- microservices/validation_service/src/routes/validation_test.py
+++ microservices/validation_service/src/routes/validation_test.py
@@ -42,5 +42,6 @@ def test_validation_api_invalid_doc_class(client_with_emulator):
   doc.save()
   url = f"{API_URL}validation_api?case_id=5-ui&uid=aSCh3o6BxjPEqjMAQhtC&"\
     "doc_class=invalid_class"
-  response = client_with_emulator.post(url)
+  with mock.patch("routes.validation.Logger"):
+    response = client_with_emulator.post(url)
   assert response.status_code == 500, "Status 500"
