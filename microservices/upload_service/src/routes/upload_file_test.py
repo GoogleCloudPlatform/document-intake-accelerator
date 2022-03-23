3ac6f528e88b95f1ac378007f8786da2e0ff8d63
Shruti Math <shruti.math@quantiphi.com>
2022-03-23 13:27:24 +0530

    logger mocked in upload_file test

--- microservices/upload_service/src/routes/upload_file_test.py
+++ microservices/upload_service/src/routes/upload_file_test.py
@@ -17,60 +17,63 @@ SUCCESS_RESPONSE = {"status": "Success"}
 
 def test_upload_json_without_case_id_positive(client_with_emulator):
   with mock.patch("routes.upload_file.create_document_from_data"):
-    response = client_with_emulator.post(
-        f"{api_url}upload_json",
-        json={
-            "name": "Jon",
-            "employer_name": "Quantiphi",
-            "employer_phone_no": "9282112222",
-            "context": "Callifornia",
-            "dob": "7 Feb 1997",
-            "document_type": "application/ supporting",
-            "document_class": "unemployment",
-            "ssn": "1234567",
-            "phone_no": "9730388333",
-            "application_apply_date": "2022/03/16",
-            "mailing_address": "Arizona USA",
-            "mailing_city": "Phoniex",
-            "mailing_zip": "123-33-22",
-            "residential_address": "Phoniex , USA",
-            "work_end_date": "2022/03",
-            "sex": "Female"
-        })
-    print(response)
-    assert response.status_code == 200
+    with mock.patch("routes.upload_file.Logger"):
+        response = client_with_emulator.post(
+            f"{api_url}upload_json",
+            json={
+                "name": "Jon",
+                "employer_name": "Quantiphi",
+                "employer_phone_no": "9282112222",
+                "context": "Callifornia",
+                "dob": "7 Feb 1997",
+                "document_type": "application/ supporting",
+                "document_class": "unemployment",
+                "ssn": "1234567",
+                "phone_no": "9730388333",
+                "application_apply_date": "2022/03/16",
+                "mailing_address": "Arizona USA",
+                "mailing_city": "Phoniex",
+                "mailing_zip": "123-33-22",
+                "residential_address": "Phoniex , USA",
+                "work_end_date": "2022/03",
+                "sex": "Female"
+            })
+        print(response)
+  assert response.status_code == 200
 
 
 def test_upload_json_with_case_id_positive(client_with_emulator):
   with mock.patch("routes.upload_file.create_document_from_data"):
-    response = client_with_emulator.post(
-        f"{api_url}upload_json",
-        json={
-            "case_id": "123A",
-            "name": "Jon",
-            "employer_name": "Quantiphi",
-            "employer_phone_no": "9282112222",
-            "context": "Callifornia",
-            "dob": "7 Feb 1997",
-            "document_type": "application/ supporting",
-            "document_class": "unemployment",
-            "ssn": "1234567",
-            "phone_no": "9730388333",
-            "application_apply_date": "2022/03/16",
-            "mailing_address": "Arizona USA",
-            "mailing_city": "Phoniex",
-            "mailing_zip": "123-33-22",
-            "residential_address": "Phoniex , USA",
-            "work_end_date": "2022/03",
-            "sex": "Female"
-        })
-    print(response)
-    assert response.status_code == 200
+    with mock.patch("routes.upload_file.Logger"):
+        response = client_with_emulator.post(
+            f"{api_url}upload_json",
+            json={
+                "case_id": "123A",
+                "name": "Jon",
+                "employer_name": "Quantiphi",
+                "employer_phone_no": "9282112222",
+                "context": "Callifornia",
+                "dob": "7 Feb 1997",
+                "document_type": "application/ supporting",
+                "document_class": "unemployment",
+                "ssn": "1234567",
+                "phone_no": "9730388333",
+                "application_apply_date": "2022/03/16",
+                "mailing_address": "Arizona USA",
+                "mailing_city": "Phoniex",
+                "mailing_zip": "123-33-22",
+                "residential_address": "Phoniex , USA",
+                "work_end_date": "2022/03",
+                "sex": "Female"
+            })
+        print(response)
+  assert response.status_code == 200
 
 
 def test_upload_file_json_negative(client_with_emulator):
   with mock.patch("routes.upload_file.create_document_from_data"):
-    response = client_with_emulator.post(
+    with mock.patch("routes.upload_file.Logger"):
+      response = client_with_emulator.post(
         f"{api_url}upload_json",
         json={
             "case_id": "123A",
