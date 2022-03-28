edece987ce03fa109996d1b372e1f4cbacd3e868
Shruti Math <shruti.math@quantiphi.com>
2022-03-28 15:32:46 +0530

    client with emulator added

--- common/src/common/models/document_test.py
+++ common/src/common/models/document_test.py
@@ -3,13 +3,16 @@ Unit Tests for Document ORM object
 """
 
 from common.models import Document
+# disabling pylint rules that conflict with pytest fixtures
+# pylint: disable=unused-argument,redefined-outer-name,unused-import
+from testing.fastapi_fixtures import client_with_emulator
+from common.testing.firestore_emulator import firestore_emulator, clean_firestore
 
 
-def test_new_document_status():
+def test_new_document_status(client_with_emulator):
   # unit test for document ORM
   case_id = "test_id123"
   document = Document()
   document.case_id = case_id
-  document.validation_score = 0.9
   document.save()
   assert document.case_id == case_id
