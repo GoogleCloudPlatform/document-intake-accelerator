66d60f8d51a4682f75e3f15e8aeda86b0f865739
Shruti Math <shruti.math@quantiphi.com>
2022-03-28 15:44:35 +0530

    firestore emulator added in document_test.py

--- common/src/common/models/document_test.py
+++ common/src/common/models/document_test.py
@@ -4,15 +4,14 @@ Unit Tests for Document ORM object
 
 from common.models import Document
 # disabling pylint rules that conflict with pytest fixtures
-# pylint: disable=unused-argument,redefined-outer-name,unused-import
-from testing.fastapi_fixtures import client_with_emulator
+# pylint: disable=unused-argument,redefined-outer-name,unused-import,ungrouped-imports
 from common.testing.firestore_emulator import firestore_emulator, clean_firestore
 
 
-def test_new_document_status(client_with_emulator):
+def test_new_document_status(firestore_emulator):
   # unit test for document ORM
   case_id = "test_id123"
-  document = Document()
+  document = Document(firestore_emulator)
   document.case_id = case_id
   document.save()
   assert document.case_id == case_id
