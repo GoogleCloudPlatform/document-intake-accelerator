94b1b0611cd622b47edbe40e91ef5bfc5c2b5590
shamli.ingole <shamli.ingole@quantiphi.com>
2022-04-04 12:22:24 +0530

    Resolved lint issues

--- microservices/upload_service/src/routes/process_task.py
+++ microservices/upload_service/src/routes/process_task.py
@@ -15,10 +15,8 @@ FAILED_RESPONSE = {"status": "Failed"}
 
 
 @router.post("/process_task", status_code=status.HTTP_202_ACCEPTED)
-async def process_task(payload: ProcessTask, background_task: BackgroundTasks
-, response: Response):
+async def process_task(payload: ProcessTask, background_task: BackgroundTasks):
   """Runs the Pipeline to process the document"""
-  Logger.info("=======Process task called===========================")
   payload = payload.dict()
   case_id = payload.get("case_id")
   uid = payload.get("uid")
@@ -44,11 +42,7 @@ def run_pipeline(case_id: str, uid: str, gcs_url: str, is_hitl: bool = False
   try:
     if not is_hitl:
       cl_result = get_classification(case_id, uid, gcs_url)
-      print("====================classification_status===================",
-          cl_result.json(), cl_result.status_code)
       if cl_result.status_code == 200:
-        print(
-          "===============classification successful======================", cl_result.json())
         Logger.info("Classification successful")
         document_type = cl_result.json().get("doc_type")
         document_class = cl_result.json().get("doc_class")
@@ -57,46 +51,27 @@ def run_pipeline(case_id: str, uid: str, gcs_url: str, is_hitl: bool = False
              document_class:{document_class}")
 
     if is_hitl or cl_result.status_code == 200:
-      print("===============Start Extraction for======================",
-          document_type, document_class)
       extract_res = get_extraction_score(
         case_id, uid, gcs_url, document_class)
-      
-      print("===extract_res,extract_res.status_code,document_type====",
-          extract_res, extract_res.status_code, document_type)
-      if extract_res.status_code is 200 and document_type == "application_form":
+      if extract_res.status_code == 200 and \
+        document_type == "application_form":
         extraction_score = extract_res.json().get("score")
         autoapproval_status = get_values(
                 validation_score, extraction_score, matching_score,
                 document_class, document_type)
         Logger.info(
           f"autoapproval_status for application:{autoapproval_status}")
-        print("===========autoapproval status===========",
-            autoapproval_status)
         update_autoapproval_status(
-          case_id, uid, "success", autoapproval_status[0],
-            "yes")
-        print(
-          "===============Extraction successful for application_form======================",autoapproval_status)
-      elif extract_res.status_code == 200 and document_type == "supporting_documents":
+          case_id, uid, "success", autoapproval_status[0],"yes")
+      elif extract_res.status_code == 200 and \
+        document_type == "supporting_documents":
         extraction_score = extract_res.json().get("score")
-        print("===============Extraction successful for supporting_document======================",
-            extract_res, document_class)
-        validation_res = get_validation_score(
-          case_id, uid, document_class)
-        print(
-          "===============Start Validation======================", validation_res)
+        validation_res = get_validation_score(case_id, uid, document_class)
         if validation_res.status_code == 200:
           validation_score = validation_res.json().get("score")
-          print(
-            "===============Validation successful======================", validation_res.json())
           matching_res = get_matching_score(case_id, uid)
-          print("============matching_res============",
-              matching_res.json())
           if matching_res.status_code == 200:
             matching_score = matching_res.json().get("score")
-            print(
-              "===============Matching successful======================", matching_res)
             try:
               Logger.info(f"extraction_score:{extraction_score}")
               Logger.info(f"validation_score:{validation_score}")
@@ -106,20 +81,18 @@ def run_pipeline(case_id: str, uid: str, gcs_url: str, is_hitl: bool = False
                 document_class, document_type)
               Logger.info(
                 f"autoapproval_status:{autoapproval_status}")
-              print("===========autoapproval status===========",
-                  autoapproval_status)
               update_autoapproval_status(
-                case_id, uid, "success", autoapproval_status[0],
-                 "yes")
+                case_id, uid, "success", autoapproval_status[0],"yes")
             except Exception as e:
-              err = traceback.format_exc().replace('\n', ' ')
+              Logger.error(e)
+              err = traceback.format_exc().replace("\n", " ")
               Logger.error(f"Error in Autoapproval: {err}")
               update_autoapproval_status(
                 case_id, uid, "fail", None, None)
 
             Logger.info("Process task executed SUCCESSFULLY.")
           else:
-            err = traceback.format_exc().replace('\n', ' ')
+            err = traceback.format_exc().replace("\n", " ")
             Logger.error(err)
             Logger.error("Matching FAILED")
         else:
@@ -128,16 +101,16 @@ def run_pipeline(case_id: str, uid: str, gcs_url: str, is_hitl: bool = False
           Logger.error("Validation FAILED")
 
     else:
-      Logger.error(f"Classification FAILED.")
+      Logger.error("Classification FAILED.")
     return SUCCESS_RESPONSE
   except Exception as e:
-    err = traceback.format_exc().replace('\n', ' ')
+    err = traceback.format_exc().replace("\n", " ")
     Logger.error(err)
     raise HTTPException(status_code=500, detail=e)
 
 
 def get_classification(case_id: str, uid: str, gcs_url: str):
-  """Call the classification API and get the type and class of 
+  """Call the classification API and get the type and class of
   the document"""
   base_url = "http://classification-service/classification_service/v1/"\
     "classification/classification_api"
@@ -147,7 +120,7 @@ def get_classification(case_id: str, uid: str, gcs_url: str):
   return response
 
 
-def get_extraction_score(case_id: str, uid: str, gcs_url: str, document_class: str):
+def get_extraction_score(case_id: str, uid: str, document_class: str):
   """Call the Extraction API and get the extraction score"""
   base_url = "http://extraction-service/extraction_service/v1/extraction_api"
   req_url = f"{base_url}?case_id={case_id}&uid={uid}" \
@@ -175,13 +148,14 @@ def get_matching_score(case_id: str, uid: str):
   return response
 
 
-def update_autoapproval_status(case_id: str, uid: str, status: str,
+def update_autoapproval_status(case_id: str, uid: str, a_status: str,
                  autoapproved_status: str, is_autoapproved: str):
   """Update auto approval status"""
   base_url = "http://document-status-service/document_status_service" \
     "/v1/update_autoapproved_status"
   req_url = f"{base_url}?case_id={case_id}&uid={uid}" \
-    f"&status={status}&autoapproved_status={autoapproved_status}&is_autoapproved={is_autoapproved}"
+    f"&status={a_status}&autoapproved_status={autoapproved_status}"\
+      "&is_autoapproved={is_autoapproved}"
   response = requests.post(req_url)
   return response
 
