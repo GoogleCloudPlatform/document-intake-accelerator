c7433512eddeaa87501e1dc0d62ebbfe8ec5b55a
venkatp-krishna <venkata.panga@qunatiphi.com>
2022-04-11 14:20:13 +0530

    lint8

--- microservices/extraction_service/src/utils/change_json_format.py
+++ microservices/extraction_service/src/utils/change_json_format.py
@@ -1,9 +1,8 @@
-import json
-from collections import ChainMap
+"""
+  This module used to format response
 
-'''
-Input json format
-json=[{
+  Input json format
+  json=[{
         "entity": "name",
         "value": "Kathr1n    marie",
         "key_confidence": 1.0,
@@ -27,53 +26,51 @@ json=[{
         "key_confidence": 1.0,
         "value_confidence": 1.0
     },
-   
     ]
+"""
 
-
-'''
-
+from collections import ChainMap
 
 def get_json_format_for_processing(input_json):
-    '''Function to change list of dictionary json format to key value mapping dictionary
-    Input:
-        input_json: input json with list of dictionary format
-    Output:
-        new_json: list of key value mapping dictionary'''
+  """Function to change list of dictionary json format to key value mapping
+        dictionary
+  Input:
+    input_json: input json with list of dictionary format
+  Output:
+    new_json: list of key value mapping dictionary"""
 
-    a = input_json
-    new_list = []
-    # for dictionary in input json
-    for i in a:
-        # get list of keys for the dictionary
-        a = {}
-        # create a dictionary from input dictionary
-        a[i.get("entity")] = i.get("value")
-        # append the new dictionary in a list
-        new_list.append(a)
-    res = {}
-    # convert list to dictionary
-    res = dict(ChainMap(*new_list))
-    # get new_json as list of dictionary
-    new_json = [res]
-    return new_json
+  a = input_json
+  new_list = []
+  # for dictionary in input json
+  for i in a:
+    # get list of keys for the dictionary
+    a = {}
+    # create a dictionary from input dictionary
+    a[i.get("entity")] = i.get("value")
+    # append the new dictionary in a list
+    new_list.append(a)
+  res = {}
+  # convert list to dictionary
+  res = dict(ChainMap(*new_list))
+  # get new_json as list of dictionary
+  new_json = [res]
+  return new_json
 
 
 def correct_json_format_for_db(output_dict, input_json):
-    '''Function to add a list of dictionary for key value mapping
-    to input list of dictionary json
-    Input:
-        output_dict: list of dictionary with key value mapping
-        input_json: list of dictionary json
-    Output:
-        input_json: list of dictionary json'''
-    # traverse input json
-    for item in input_json:
-        # traverse the keys in dictionary
-        for entity in output_dict[0].keys():
-            # if keys are matched
-            if item.get("entity") == entity:
-                # reassign input json value to new one
-                item["value"] = output_dict[0][entity]
-   
-    return input_json
+  """Function to add a list of dictionary for key value mapping
+       to input list of dictionary json
+  Input:
+    output_dict: list of dictionary with key value mapping
+    input_json: list of dictionary json
+  Output:
+    input_json: list of dictionary json"""
+  # traverse input json
+  for item in input_json:
+    # traverse the keys in dictionary
+    for entity in output_dict[0].keys():
+      # if keys are matched
+      if item.get("entity") == entity:
+        # reassign input json value to new one
+        item["value"] = output_dict[0][entity]
+  return input_json
--- microservices/extraction_service/src/utils/post_processing_config.py
+++ microservices/extraction_service/src/utils/post_processing_config.py
@@ -1,6 +1,7 @@
 '''
 This is a config file for post-processing script.
-If the user wants to post process any extracted field value he can make changes to this file.
+If the user wants to post process any extracted
+    field value he can make changes to this file.
 The post processing script handles the following:
 1. Cleans any noise in the extracted value.
 2. Converts a value to its upper case
@@ -16,7 +17,8 @@ For date formats user can use below symbols:
 %B Month as locale’s full name eg.January, February, …, December (en_US);
 %m Month as a zero-padded decimal number eg:01, 02, …, 12
 %y Year without century as a zero-padded decimal number eg:00, 01, …, 99
-%Y Year with century as a decimal number eg:0001, 0002, …, 2013, 2014, …, 9998, 9999
+%Y Year with century as a decimal number eg:0001, 0002, …,
+2013, 2014, …, 9998, 9999
 eg: 03/04/2022 is '%m/%d/%y'
 '''
 # Template to be used to convert to string(based on observation).
@@ -32,8 +34,10 @@ str_to_num_dict = {'B': '8', 'O': '0', 'I': '1', 'A': '4', 'S': '5'}
 # dictionary to clean extracted values.
 # eg: if input_dict={'dob':'2021-12-04\n'} then we have noise='\n'
 # So we pass clean_value_dict={'dob':'\n'}
-# In case dob key has different noise for different document create clean_value_dict={'dob':['noise1','noise2']}
-clean_value_dict = {'dob': ['\n', 'IM'], 'application_apply_date': ['\n'], 'effective_date': ['\n'],
+# In case dob key has different noise for different document create
+# clean_value_dict={'dob':['noise1','noise2']}
+clean_value_dict = {'dob': ['\n', 'IM'], 'application_apply_date': ['\n'],
+                    'effective_date': ['\n'],
                     'employer_address': ['\n'], 'employer_county': ['\n'],
                     'employer_name': ['\n']}
 
@@ -50,14 +54,18 @@ upper_to_lower_list = ['address']
 clean_space_list = ['name']
 
 # dictionary to update date format.
-# The dictionary key values are the key field names and the dictionary values
-# are list of two input items. first item is input_date_format and second item is output date format.
+# The dictionary key values are the key field names and the
+# dictionary values
+# are list of two input items. first item is input_date_format and
+# second item is output date format.
 # date_format_dict = {'dob': ['%Y-%m-%d', '%y/%m/%d']}
 date_format_dict = {'dob': ['%Y-%m-%d', '%Y-%m-%d']}
-# list of key fields for which extracted values are needed to be corrected to string
+# list of key fields for which extracted values are needed to be
+# corrected to string
 # will be based on num_to_str_dict
 convert_to_string = ['name']
 
-# list of key fields for which extracted values are needed to be corrected to integer
+# list of key fields for which extracted values are needed to be
+# corrected to integer
 # will be based on str_to_num_dict.
 convert_to_number = ['phone_no']
