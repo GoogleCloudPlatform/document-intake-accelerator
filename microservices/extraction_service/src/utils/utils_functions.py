cf761c722d1cbe628ac6e81daffb5e4d5cb6df6a
venkatp-krishna <venkata.panga@qunatiphi.com>
2022-04-18 17:40:36 +0530

    logging

--- microservices/extraction_service/src/utils/utils_functions.py
+++ microservices/extraction_service/src/utils/utils_functions.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 from functools import reduce
 from google.cloud import storage
+from common.utils.logging_handler import Logger
 
 def pattern_based_entities(parser_data, pattern):
   """
@@ -203,6 +204,7 @@ def entities_extraction(parser_data, required_entities, doc_type):
   # Extract default entities
   entity_dict = default_entities_extraction(parser_entities,
                                             default_entities,doc_type)
+  Logger.info("Default entities created from Specialized parser response")
   # if any derived entities then extract them
   if derived_entities:
     # this function can be used for all docs, if derived entities
@@ -210,6 +212,7 @@ def entities_extraction(parser_data, required_entities, doc_type):
     derived_entities_extracted_dict = derived_entities_extraction\
         (parser_data, derived_entities)
     entity_dict.update(derived_entities_extracted_dict)
+    Logger.info("Derived entities created from Specialized parser response")
   return entity_dict
 
 
@@ -343,6 +346,7 @@ def standard_entity_mapping(desired_entities_list, parser_name):
   df_final = df_final.replace(r"^\s*$", np.nan, regex=True)
   df_final = df_final.replace({np.nan: None})
   extracted_entities_final_json = df_final.to_dict("records")
+  Logger.info("Entities standardization completed")
   return extracted_entities_final_json
 
 
@@ -394,7 +398,8 @@ def form_parser_entities_mapping(form_parser_entity_list, mapping_dict,
              "page_height": int(df["page_height"][idx_list[idx]])
              }
         except: # pylint: disable=bare-except
-          print("If key doesn't present in response")
+          Logger.info("Key not found in parser output,"
+                      " so filling null value")
           temp_dict = {"entity": each_val, "value": None,
                        "extraction_confidence": None,
                        "manual_extraction": False,
@@ -420,6 +425,7 @@ def form_parser_entities_mapping(form_parser_entity_list, mapping_dict,
                          "page_height": None
                          }
         required_entities_list.append(temp_dict)
+  Logger.info("Default entities created from Form parser response")
   if derived_entities:
     # this function can be used for all docs, if derived entities
     # are extracted by using regex pattern
@@ -428,6 +434,7 @@ def form_parser_entities_mapping(form_parser_entity_list, mapping_dict,
     derived_entities_op_dict = derived_entities_extraction(parser_data,
                                                            derived_entities)
     required_entities_list.extend(list(derived_entities_op_dict.values()))
+    Logger.info("Derived entities created from Form parser response")
 
   return required_entities_list,flag
 
@@ -485,7 +492,7 @@ def clean_form_parser_keys(text):
     if last_word in [")", "]"]:
       text += last_word
   except: # pylint: disable=bare-except
-    print("Exception occurred while processing")
+    Logger.error("Exception occurred while cleaning keys")
   return text
 
 def del_gcs_folder(bucket, folder):
