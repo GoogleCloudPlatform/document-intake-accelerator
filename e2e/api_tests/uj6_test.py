284d1853ad2398555faea9850e9c7a1d76788cc1
Mohit Krishnani <mohit.krishnani@quantiphi.com>
2022-04-18 22:05:23 +0530

    corrected user journey endpoint

--- e2e/api_tests/uj6_test.py
+++ e2e/api_tests/uj6_test.py
@@ -9,7 +9,7 @@ from endpoint_proxy import get_baseurl
 
 #Accessing ping endpoint for sample service
 
-def test_api_ping():
+def test_report_data_api():
   base_url = get_baseurl("hitl-service")
-  res = requests.get(base_url + "/report_data")
+  res = requests.get(base_url + "/hitl_service/v1/report_data")
   assert res.status_code == 200
