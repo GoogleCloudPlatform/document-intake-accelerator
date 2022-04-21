4a1b6cf4942109049db747a9af39c717fa9b6df2
shamli.ingole <shamli.ingole@quantiphi.com>
2022-04-21 19:16:14 +0530

    changed fixture scope

--- e2e/api_tests/conftest.py
+++ e2e/api_tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from setup_e2e import create_table, delete_dataset
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def setup():
   print("=============CREATING TABLE=============")
   create_table()
