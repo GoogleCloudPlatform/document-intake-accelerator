2f5ee8d10ba2c6e80abc00fc27722ab0594366c9
Mohit Krishnani <mohit.krishnani@quantiphi.com>
2022-04-22 16:03:20 +0530

    Updated uj 6 and 8

--- e2e/api_tests/uj6_test.py
+++ e2e/api_tests/uj6_test.py
@@ -4,11 +4,30 @@
   is working
 """
 
+import datetime
 import requests
 from endpoint_proxy import get_baseurl
+from common.models.document import Document
 
-
-def test_report_data_api():
+def test_all_table_data():
+  timestamp = str(datetime.datetime.utcnow())
+  d = Document()
+  d.case_id = "test_1"
+  d.uid = "utest_1"
+  d.active = "active"
+  d.upload_timestamp = timestamp
+  d.system_status = [{
+    "stage":"uploaded",
+    "status":"success",
+    "timestamp":timestamp
+  }]
+  d.save()
   base_url = get_baseurl("hitl-service")
   res = requests.get(base_url + "/hitl_service/v1/report_data")
   assert res.status_code == 200
+
+  res_data = res.json()
+  print(res_data)
+
+  assert res_data["len"] > 0
+  assert res_data["data"] is not []
--- e2e/api_tests/uj8_test.py
+++ e2e/api_tests/uj8_test.py
@@ -5,11 +5,59 @@
 
 import requests
 from endpoint_proxy import get_baseurl
+import datetime
+from common.models.document import Document
 
+def add_records():
+  timestamp = str(datetime.datetime.utcnow())
+  d = Document()
+  d.case_id = "test_1"
+  d.uid = "utest_1"
+  d.active = "active"
+  d.upload_timestamp = timestamp
+  d.auto_approval = "Approved"
+  d.system_status = [{
+    "stage":"uploaded",
+    "status":"success",
+    "timestamp":timestamp
+  }]
+  d.save()
 
+  d = Document()
+  d.case_id = "test_2"
+  d.uid = "utest_2"
+  d.active = "active"
+  d.upload_timestamp = timestamp
+  d.auto_approval = "Rejected"
+  d.hitl_status = [{
+    "status":"approved",
+    "user":"John Adam",
+    "comment":"Approving",
+    "timestamp":str(datetime.datetime.utcnow()+datetime.timedelta(seconds=3))
+  }]
+  d.system_status = [{
+    "stage":"uploaded",
+    "status":"success",
+    "timestamp":timestamp
+  },
+  {
+    "stage":"auto_approval",
+    "status":"success",
+    "timestamp":timestamp
+  }]
+  d.save()
+
+def delete_records():
+  Document().collection.delete_all()
 def test_approved_queue():
+  add_records()
   base_url = get_baseurl("hitl-service")
   status="approved"
   res = requests.post(base_url + f"/hitl_service/v1/get_queue?"\
     f"hitl_status={status}")
   assert res.status_code == 200
+  res_data = res.json()
+  print(res_data)
+  assert res_data["len"]>0
+  assert res_data["data"] is []
+  delete_records()
