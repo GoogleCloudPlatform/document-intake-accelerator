40473813c786dafa1e5e7cf39015d902c90fe5db
Shruti Math <shruti.math@quantiphi.com>
2022-04-24 16:29:19 +0530

    user journey for reassign and upload jso added

--- e2e/api_tests/conftest.py
+++ e2e/api_tests/conftest.py
@@ -1,8 +1,12 @@
 import pytest
 from setup_e2e import create_test_table, delete_dataset
+#create_bucket_class_location
 
 @pytest.fixture(scope="session")
 def setup():
   create_test_table()
+  # create_bucket_class_location()
   yield
   delete_dataset()
+
+
