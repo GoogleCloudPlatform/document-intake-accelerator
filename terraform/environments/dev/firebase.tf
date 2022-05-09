d5a98763f4efdbf03e0cbe5eb342f77917f3f7e9
Jon Chen <jonchen@google.com>
2022-05-09 17:37:08 -0400

    fixed Firestore region

--- terraform/environments/dev/firebase.tf
+++ terraform/environments/dev/firebase.tf
@@ -3,6 +3,6 @@
 resource "google_app_engine_application" "firebase_init" {
   provider = google-beta
   project  = var.project_id
-  location_id = var.region
+  location_id = var.firestore_region
   database_type = "CLOUD_FIRESTORE"
 }
