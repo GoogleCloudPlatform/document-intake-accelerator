978d0c3ad0b9e68d2959ae1591bc95eedac65cdf
Jon Chen <jonchen@google.com>
2022-05-09 14:56:48 -0400

    fixed firebase.tf

--- terraform/environments/dev/firebase.tf
+++ terraform/environments/dev/firebase.tf
@@ -2,7 +2,7 @@
 
 resource "google_app_engine_application" "app" {
   provider = google-beta
-  project  = google_project.default.project_id
+  project  = var.project_id
   location_id = var.region
   database_type = "CLOUD_FIRESTORE"
 }
