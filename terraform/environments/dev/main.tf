99b20c13247003a8f92e8010549a27b7d3e5f280
Jon Chen <jonchen@google.com>
2022-05-10 17:52:18 -0400

    updated firebase backup bucket

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -240,35 +240,6 @@ resource "google_storage_bucket" "assets" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "firestore-backup-bucket" {
-  name          = "${local.project_id}-firestore-backup"
-  location      = local.multiregion
-  storage_class = "NEARLINE"
-
-  uniform_bucket_level_access = true
-
-  lifecycle_rule {
-    condition {
-      age = 356
-    }
-    action {
-      type = "Delete"
-    }
-  }
-}
-
-# give backup SA rights on bucket
-resource "google_storage_bucket_iam_binding" "firestore_sa_backup_binding" {
-  bucket = google_storage_bucket.firestore-backup-bucket.name
-  role   = "roles/storage.admin"
-  members = [
-    "serviceAccount:${local.project_id}@appspot.gserviceaccount.com",
-  ]
-  depends_on = [
-    google_app_engine_application.firestore
-  ]
-}
-
 resource "google_compute_router" "router" {
   name    = "${local.project_id}-router"
   region  = local.region
