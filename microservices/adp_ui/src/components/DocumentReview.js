83d086665ea245649384d7df42782071667c68e9
Jon Chen <jonchen@google.com>
2022-09-06 16:51:28 -0400

    added license header to JS, CSS and HTML

--- microservices/adp_ui/src/components/DocumentReview.js
+++ microservices/adp_ui/src/components/DocumentReview.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /** This page renders a PDF view and the extracted values in the inputboxes. Here there are two cases
  * a) PDF with the extracted values
  * b) If the document is unclassifed, a user can manually classify by selected the doument class
--- microservices/adp_ui/src/components/DragDropUpload.js
+++ microservices/adp_ui/src/components/DragDropUpload.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /** This page is a subpage for Upload.js where the drag and drop feature is listed here. A user can either drag
  * or drop the files else select on the window to open a popup from which the files are selected
  */
