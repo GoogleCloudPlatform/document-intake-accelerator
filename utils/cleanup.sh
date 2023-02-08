638e1050dcb36ff387c2d638cef87f13a5c3ee81
evekhm <evekhm@gmail.com>
2023-02-08 15:41:50 -0800

    - Improved cleanup utility

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR"/../SET
+
+echo "Cleaning Up environment..."
 "$DIR"/bq_cleanup.sh
 "$DIR"/database_cleanup.sh
-
 "$DIR"/gcs_cleanup.sh "gs://${PROJECT_ID}-pa-forms"
 "$DIR"/gcs_cleanup.sh "gs://${PROJECT_ID}-document-upload"
 "$DIR"/gcs_cleanup.sh "gs://${PROJECT_ID}-docai-output"
