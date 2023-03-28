b6eff54ee117cdc440a6d67173f670e96c31b290
evekhm <evekhm@gmail.com>
2023-03-27 19:43:48 -0700

    - Var fix

--- setup/setup_host_vpc_project.sh
+++ setup/setup_host_vpc_project.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 export CLOUD_ROUTER_NAME=cda-router
 export CLOUD_NAT_NAME=cda-nat
 export REGION=us-central1
@@ -5,25 +7,25 @@ export FW_RULE_NAME=fw-i-a-gkemaster-gkeworkers-tcp-8443-webhook
 export MASTER_IPV4_CIDR="172.16.0.0/28" # default range if not changed in terraform.tfvars
 export GKE_NETWORK_TAG=gke-main-cluster
 
-export NET=cda-vpc
+export NETWORK=cda-vpc
 export SUBNET1=tier-1
 export SUBNET2=tier-2
 
 gcloud services enable container.googleapis.com --project $HOST_PROJECT_ID
-gcloud compute networks create $NET \
+gcloud compute networks create $NETWORK \
     --subnet-mode custom \
     --project $HOST_PROJECT_ID
 
 gcloud compute networks subnets create $SUBNET1 \
     --project $HOST_PROJECT_ID \
-    --network $NET \
+    --network $NETWORK \
     --range 10.0.4.0/22 \
     --region us-central1 \
     --secondary-range tier-1-services=10.0.32.0/20,tier-1-pods=10.4.0.0/14
 
 gcloud compute networks subnets create $SUBNET2 \
     --project $HOST_PROJECT_ID \
-    --network $NET \
+    --network $NETWORK \
     --range 172.16.4.0/22 \
     --region us-central1 \
     --secondary-range tier-2-services=172.16.16.0/20,tier-2-pods=172.20.0.0/14
