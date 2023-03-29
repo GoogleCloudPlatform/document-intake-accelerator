42fcc01aba65db15a0473a025c958386525dad13
evekhm <evekhm@gmail.com>
2023-03-29 13:19:53 -0700

    - Swagger Utility

--- setup/deploy_endpoint.sh
+++ setup/deploy_endpoint.sh
@@ -7,6 +7,7 @@ if [ -z "$K8S_INGRESS_IP" ]; then
   echo "must provide Reserved IP address as a parameter"
 fi
 
+echo "Deploying end point ${FQDN} with reserved IP $K8S_INGRESS_IP"
 cat <<EOF > ${K8S_NAME}-openapi.yaml
 swagger: "2.0"
 info:
@@ -23,3 +24,6 @@ EOF
 
 gcloud endpoints services deploy ${K8S_NAME}-openapi.yaml
 rm ${K8S_NAME}-openapi.yaml
+export API_DOMAIN=${FQDN}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${DIR}"/../SET
