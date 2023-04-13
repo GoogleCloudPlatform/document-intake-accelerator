bd64d64938a7948a0c052a4823bd98417587e719
evekhm <evekhm@gmail.com>
2023-04-13 15:30:45 -0700

    - Added SQL scripts

--- sql-scripts/query_corrected_values.sql
+++ sql-scripts/query_corrected_values.sql
@@ -4,9 +4,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,9 +16,8 @@
 
 /*
  * Use Cases:
- * - Display All extracted entities (if there are multiple entries for the same uid, shows the latest entry)
+ * - Display All corrected values from HITL process
  */
-
 
 WITH latest AS (
     SELECT *
@@ -28,10 +27,13 @@ WITH latest AS (
     flattened_1 AS (
 SELECT *,
     JSON_VALUE(parent, "$.corrected_value") as corrected_value,
+    JSON_VALUE(parent, "$.value") as value,
     CAST(JSON_VALUE(parent, "$.confidence") AS DECIMAL) as confidence,
     JSON_VALUE(parent, "$.name") as name,
-    JSON_VALUE(parent, "$.value") as value,
+
 FROM latest l
     CROSS JOIN UNNEST(JSON_QUERY_ARRAY(entities, "$")) parent
     )
-SELECT * except (entities, parent) FROM flattened_1
+SELECT * except (entities, parent, document_type) FROM flattened_1
+WHERE corrected_value != ""
+ORDER BY flattened_1.confidence ASC
--- sql-scripts/run_query.sh
+++ sql-scripts/run_query.sh
@@ -3,8 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/../SET"
 
+SCRIPT=$1
+
+echo "Running $SCRIPT"
+
 BIGQUERY_DATASET=validation
-bq query --project_id="$PROJECT_ID" --dataset_id=$BIGQUERY_DATASET --nouse_legacy_sql  --flagfile="${DIR}"/query_extracted_entities.sql
+bq query --project_id="$PROJECT_ID" --dataset_id=$BIGQUERY_DATASET --nouse_legacy_sql  --flagfile="${DIR}/${SCRIPT}"
 
 #bq query --project_id="$PROJECT_ID" --dataset_id=$BIGQUERY_DATASET --nouse_legacy_sql  --flagfile="${DIR}"/query_extraction_confidence.sql
 
