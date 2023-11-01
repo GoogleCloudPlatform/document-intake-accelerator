00a538258a0b44ba8688b9ec660861b8442d2723
evekhm <evekhm@gmail.com>
2023-11-01 15:17:52 -0700

    minor improvements

--- utils/docai-wh/genai_search.sh
+++ utils/docai-wh/genai_search.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#QUESTION="Tell me about Linda Ortiz?"
-
 if [[ -z "${DOCAI_WH_PROJECT_ID}" ]]; then
   echo DOCAI_WH_PROJECT_ID env variable is not set.
   exit
@@ -13,9 +11,10 @@ PROJECT_NUM=$(gcloud projects describe "$DOCAI_WH_PROJECT_ID" --format='get(proj
 ENDPOINT="https://contentwarehouse.googleapis.com/v1/projects/$PROJECT_NUM/locations/us/documents:search"
 
 echo "Please input your question below:"
+#QUESTION="Tell me about Linda Ortiz?"
 read user_question
 
-# To identify the documents which are close/relevant to our natural-language search query
+# To identify the documents which are close/relevant to the natural-language search query
 # (no of documents can be controlled with the field - qa_size_limit)
 REQUEST='{
   "document_query":
@@ -38,12 +37,10 @@ curl -s -X POST \
   -d "$REQUEST" \
   $ENDPOINT > temp.json
 
-#cat temp.json | jq -r '.matchingDocuments[] | "\(.document.name),\(.document.documentSchemaName)"' > documents.txt
-##curl -X POST -H "Authorization: Bearer $(gcloud auth application-default print-access-token)"  -H "Content-Type: application/json; charset=utf-8" https://contentwarehouse.googleapis.com/v1/projects/514064100333/locations/us/documents:search -d ' { "document_query": {"query": "What is FINRA Dispute Resolution Service?","is_nl_query": "true","document_name_filter" : ["projects/514064100333/locations/us/documents/51iq892ktqibg"]}, "qa_size_limit": "1","request_metadata": { "user_info": {"id": "user:rpallapolu@google.com"}} }
-
 cat temp.json | jq -r '.matchingDocuments[] | "\"\(.document.name)\","'  > documents.txt
 
-
+#  Once we have the document id's  we need to run the query again limiting to those document/documents
+#  based on confidence_score
 DOCUMENTS=$(cat documents.txt)
 REQUEST='{
    "document_query": {
